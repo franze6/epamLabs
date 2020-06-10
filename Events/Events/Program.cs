8523b82dce8ea2bdafa7a6a03cbfaf9d70fe9559
Kirill A. Artemovich <kandrusyak@tsconsulting.com>
2020-06-10 19:53:42 +0300

    fix events

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -12,6 +12,22 @@ namespace Events
         public delegate void OnLeave(Person p);
         static void Main(string[] args)
         {
+            List<Person> listPersons = new List<Person>();
+            listPersons.Add(new Person { Name = "Кирилл" });
+            listPersons.Add(new Person { Name = "Никита" });
+            listPersons.Add(new Person { Name = "Юля" });
+            listPersons.Add(new Person { Name = "Илюха" });
+            Office office = new Office(listPersons);
+            foreach (var p in listPersons)
+            {
+                p.Came();
+            }
+            foreach (var p in listPersons)
+            {
+                p.Leave();
+            }
+
+            Console.ReadKey();
         }
     }
 }
